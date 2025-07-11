0ae09e92e424526c68627a297ab4c8b96093516c
mathcorreia <202409183211@alunos.unimetrocamp.edu.br>
2025-07-11 20:21:23 -0300

    3

--- frontend/web/src/app.tsx
+++ frontend/web/src/app.tsx
@@ -1,11 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { CreateRoom } from './pages/createRoom';
-import { RecordRoomAudio } from './pages/record-room-audip';
-import { Room } from './pages/room';
-export function App() {
-  const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Room } from './pages/room'
+import { CreateRoom } from './pages/createRoom'
+import { RecordRoomAudio } from './pages/record-room-audip'
 
+const queryClient = new QueryClient()
+
+export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
@@ -16,5 +17,5 @@ export function App() {
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  );
+  )
 }
--- frontend/web/src/pages/room.tsx
+++ frontend/web/src/pages/room.tsx
@@ -1,18 +1,18 @@
-import { ArrowLeft, Radio } from 'lucide-react';
-import { Link, Navigate, useParams } from 'react-router-dom';
-import { QuestionList } from '@/components/question-list';
-import { Button } from '@/components/ui/button';
-import { QuestionForm } from '../components/question-form';
+import { ArrowLeft, Radio } from 'lucide-react'
+import { Link, Navigate, useParams } from 'react-router-dom'
+import { QuestionForm } from '@/components/question-form'
+import { QuestionList } from '@/components/question-list'
+import { Button } from '@/components/ui/button'
 
 type RoomParams = {
-  roomId: string;
-};
+  roomId: string
+}
 
 export function Room() {
-  const params = useParams<RoomParams>();
+  const params = useParams<RoomParams>()
 
   if (!params.roomId) {
-    return <Navigate replace to="/" />;
+    return <Navigate replace to="/" />
   }
 
   return (
@@ -48,5 +48,5 @@ export function Room() {
         <QuestionList roomId={params.roomId} />
       </div>
     </div>
-  );
+  )
 }
